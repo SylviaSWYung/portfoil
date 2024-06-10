31cc74b885ea9db7c502ee97e1c8ad7971e30cad
Sylvia Yung <syung314@gmail.com>
2024-06-10 12:26:12 +0200

    fixed navbar, without meny

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,33 @@
 export function Navbar(){
+
+    const link_classname = "block py-2 px-3 text-text-pink text-3xl";
+    
     return(
-        <div className="relative h-32">
-            <h1>Sylvia Suet Wai Yung</h1>
-        </div>
+        <nav className="fixed h-20 border-b-2 border-dark-pink flex p-4 w-full">
+            <div className="flex items-center justify-between w-full mx-auto p-4">
+                <div>
+                    <h1 className="flex items-center self-center text-text-pink text-3xl tracking-wide ml-4">Sylvia Suet Wai Yung</h1>
+                </div>
+                {/* <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button>
+                        lage hamburger meny senere
+                    </button>
+                </div> */}
+                <div className="ml-auto flex items-center">
+                    <ul className="flex space-x-1">
+                        <li>
+                            <a href="#" className={link_classname}>LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href="#" className={link_classname}>GitHub</a>
+                        </li>
+                        <li>
+                            <a href="#" className={link_classname}>Resume</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+       
     )
 }
